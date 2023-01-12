16dbab5da7392da642116a695cfdea1306b56303
ZanaArezina <zana.arezina.p3@gmail.com>
2023-01-12 02:31:35 +0100

    Domaci_cas6

---
+++ src/Student.java
@@ -0,0 +1,13 @@
+public class Student {
+
+    String ime;
+    String prezime;
+    int ocena;
+
+    public Student(){
+
+    }
+    public String toString() {
+        return "Ime: " + ime + " Prezime: " + prezime + " Ocena: " + ocena;
+    }
+}
